2768cbec8ab2d41acaeb63daf8ef5b7af1653f38
MYACTIVITIES\Antonius <antonius.gress@myactivities.com>
2019-01-09 21:55:56 +0100

    Changed text

--- conf.js
+++ conf.js
@@ -12,7 +12,7 @@ exports.storage = 'sqlite';
 //exports.socksPort = 9050;
 
 exports.hub = 'byteball.org/bb';
-exports.deviceName = 'Dice bot';
+exports.deviceName = 'Error Fare Alerts Bot';
 exports.permanent_pairing_secret = '*'; // * allows to pair with any code, the code is passed as 2nd param to the pairing event handler
 exports.control_addresses = [''];
 exports.payout_address = 'WHERE THE MONEY CAN BE SENT TO';
